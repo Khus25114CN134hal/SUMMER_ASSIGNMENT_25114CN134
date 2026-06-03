a0c5760e0ede76b63e09a445f6c346553b261d81
Khus25114CN134hal <khushal77yadav@gmail.com>
2026-06-03 17:14:00 +0530

    Update prime_in_range.c

--- DAY 3/prime_in_range.c
+++ DAY 3/prime_in_range.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+int main() {
+    int start, end, i, j, flag;
+    printf("Enter start and end of range: ");
+    scanf("%d %d", &start, &end);
+    printf("Prime numbers: ");
+    for (i = start; i <= end; i++) {
+        flag = 1;
+        if (i <= 1) flag = 0;
+        for (j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag) printf("%d ", i);
+    }
+    printf("\n");
+    return 0;
+}
