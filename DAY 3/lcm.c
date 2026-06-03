117828447726b8d3be55b7546c42fd6c667b923e
Khus25114CN134hal <khushal77yadav@gmail.com>
2026-06-03 17:15:18 +0530

    Implement LCM calculation using GCD

--- DAY 3/lcm.c
+++ DAY 3/lcm.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+int main() {
+    int a, b, gcd, lcm, x, y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    x = a; y = b;
+    while (x != y) {
+        if (x > y)
+            x = x - y;
+        else
+            y = y - x;
+    }
+    gcd = x;
+    lcm = (a * b) / gcd;
+    printf("LCM = %d\n", lcm);
+    return 0;
+}
