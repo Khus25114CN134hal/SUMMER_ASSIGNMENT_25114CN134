e57a4321fb7fc49a4e217b8436cf5e9c35019a0f
Khus25114CN134hal <khushal77yadav@gmail.com>
2026-06-08 10:02:31 +0530

    Update recursive_sum_of_digits.c

--- Day 7/recursive_sum_of_digits.c
+++ Day 7/recursive_sum_of_digits.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int sumdigits(int n)
+{
+    if(n==0)
+        return 0;
+    return n%10 + sumdigits(n/10);
+}
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Sum of digits: %d\n", sumdigits(n));
+    return 0;
+}
