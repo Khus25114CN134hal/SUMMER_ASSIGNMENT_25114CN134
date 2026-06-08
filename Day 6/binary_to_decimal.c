0ffc3d414777c8bd8d97ffe481953adcecf033d1
Khus25114CN134hal <khushal77yadav@gmail.com>
2026-06-08 09:54:38 +0530

    Implement binary to decimal conversion in C

--- Day 6/binary_to_decimal.c
+++ Day 6/binary_to_decimal.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+int main()
+{
+    int n, decimal=0, base=1, temp;
+    printf("Enter binary number: ");
+    scanf("%d", &n);
+    temp=n;
+    while(temp>0)
+    {
+        int last=temp%10;
+        decimal=decimal+last*base;
+        base=base*2;
+        temp=temp/10;
+    }
+    printf("Decimal: %d\n", decimal);
+    return 0;
+}
