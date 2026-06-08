aeb073bb4800ad07168a9a482f04d7d1b6989426
Khus25114CN134hal <khushal77yadav@gmail.com>
2026-06-08 10:01:49 +0530

    Update recursive_reverse_number.c

--- Day 7/recursive_reverse_number.c
+++ Day 7/recursive_reverse_number.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+int rev=0, base=1;
+void reverse(int n)
+{
+    if(n==0)
+        return;
+    rev=rev + (n%10)*base;
+    base=base*10;
+    reverse(n/10);
+}
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    reverse(n);
+    printf("Reversed: %d\n", rev);
+    return 0;
+}
