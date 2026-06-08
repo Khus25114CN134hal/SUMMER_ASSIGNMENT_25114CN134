9869af8c45540df4f5b80fb1565431c24c1f54d4
Khus25114CN134hal <khushal77yadav@gmail.com>
2026-06-08 10:11:52 +0530

    Add character triangle program in C
    Implement character triangle printing based on user input.

--- Day 8/character_triangle.c
+++ Day 8/character_triangle.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter rows: ");
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++)
+    {
+        char ch='A';
+        for(int j=1; j<=i; j++)
+        {
+            printf("%c", ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
