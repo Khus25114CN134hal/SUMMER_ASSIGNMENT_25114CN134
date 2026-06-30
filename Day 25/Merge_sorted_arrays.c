99ce3d35a8e736df00bae46abb66ce5a09ca67fa
Khushal Yadav <khushal77yadav@gmail.com>
2026-06-30 10:29:44 +0530

    Add function to merge two sorted arrays
    Implement merge function for two sorted arrays.

--- Day 25/Merge_sorted_arrays.c
+++ Day 25/Merge_sorted_arrays.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+void mergeSortedArrays(int arr1[], int n1, int arr2[], int n2, int merged[]) {
+    int i = 0, j = 0, k = 0;
+    while (i < n1 && j < n2) {
+        if (arr1[i] <= arr2[j])
+            merged[k++] = arr1[i++];
+        else
+            merged[k++] = arr2[j++];
+    }
+    while (i < n1) merged[k++] = arr1[i++];
+    while (j < n2) merged[k++] = arr2[j++];
+}
+
+int main() {
+    int arr1[] = {1, 3, 5};
+    int arr2[] = {2, 4, 6};
+    int n1 = 3, n2 = 3;
+    int merged[6];
+
+    mergeSortedArrays(arr1, n1, arr2, n2, merged);
+
+    for (int i = 0; i < n1 + n2; i++)
+        printf("%d ", merged[i]);
+    return 0;
+}
 
