b188ab3cc0d3f30f6a94720f13d8f9341f3893bb
Khushal Yadav <khushal77yadav@gmail.com>
2026-06-30 10:31:42 +0530

    Implement function to sort names alphabetically

--- Day 25/sort_name_alphabetically.c
+++ Day 25/sort_name_alphabetically.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <string.h>
 
+void sortNames(char names[][50], int n) {
+    char temp[50];
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (strcmp(names[j], names[j + 1]) > 0) {
+                strcpy(temp, names[j]);
+                strcpy(names[j], names[j + 1]);
+                strcpy(names[j + 1], temp);
+            }
+        }
+    }
+}
+
+int main() {
+    char names[][50] = {"Charlie", "Alice", "Bob"};
+    int n = 3;
+
+    sortNames(names, n);
+
+    for (int i = 0; i < n; i++)
+        printf("%s ", names[i]);
+    return 0;
+}
