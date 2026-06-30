fe79bdbb57b64875f1d007d5b90c58b19b67d386
Khushal Yadav <khushal77yadav@gmail.com>
2026-06-30 10:30:38 +0530

    Add commonCharacters function for string comparison
    Implement function to print common characters between two strings.

--- Day 25/Characters_in_strings.c
+++ Day 25/Characters_in_strings.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+void commonCharacters(char s1[], char s2[]) {
+    int freq[256] = {0};
+    for (int i = 0; s1[i] != '\0'; i++)
+        freq[(int)s1[i]] = 1;
+
+    int seen[256] = {0};
+    for (int i = 0; s2[i] != '\0'; i++) {
+        char c = s2[i];
+        if (freq[(int)c] && !seen[(int)c]) {
+            printf("%c ", c);
+            seen[(int)c] = 1;
+        }
+    }
+}
+
+int main() {
+    char s1[] = "hello";
+    char s2[] = "world";
+    commonCharacters(s1, s2);
+    return 0;
+}
 
